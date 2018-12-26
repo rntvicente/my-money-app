6868794d029287dc2ea78bb676e6f98e387dde7a
renato.vicente <rntvicente@gmail.com>
2018-12-25 22:25:42 -0200

    Correção de mensagens

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -18,9 +18,9 @@ const database = mongoose.connect(uri, options, (err) => {
   debug('Connect database success.');
 });
 
-mongoose.Error.messages.general.required = 'O Atributo {PATH} é obrigatório.';
-mongoose.Error.messages.Number.min = 'O {VALUE} informado é menor que o limite de {MIN}.';
-mongoose.Error.messages.Number.max = 'O {VALUE} informado é maior que o limite de {MAX}.';
-mongoose.Error.messages.String.enum = '{VALUE} não é válido para o atributo {PATH}.';
+mongoose.Error.messages.general.required = 'Attribute {PATH} is required.';
+mongoose.Error.messages.Number.min = '{VALUE} is less than the limit of {MIN}.';
+mongoose.Error.messages.Number.max = '{VALUE} is greater than the limit of {MAX}.';
+mongoose.Error.messages.String.enum = '{VALUE} is not valid for {PATH} attribute.';
 
 module.exports = database;
