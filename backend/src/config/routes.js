0e70a7fc14d3d45412a5396c51f634a021b35043
renato.vicente <rntvicente@gmail.com>
2019-04-08 10:03:13 -0300

    correções

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
-const service = require('../api/billing-cycle/billing-cycle-service');
+const billingCycleService = require('../api/billing-cycle/billing-cycle-service');
+const authService = require('../api/user/auth-service');
+const auth = require('./auth');
 
 const routes = (server) => {
-  const router = express.Router();
-  server.use('/api', router);
+  const protectedApi = express.Router();
+  server.use('/api', protectedApi);
+  protectedApi.use(auth);
 
-  service.register(router, '/billingCycles');
+  billingCycleService.register(protectedApi, '/billingCycles');
+
+  const openApi = express.Router();
+  server.use('/oapi', openApi);
+
+  openApi.post('/login', authService.login);
+  openApi.post('singup', authService.singup);
+  openApi.post('validateToken', authService.validateToken);
 };
 
 module.exports = routes;
